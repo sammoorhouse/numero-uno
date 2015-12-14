38c7f0d28008d5283f8937a0fbbc60827dbcd871
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-12-14 19:05:30 -0300

    Initial Commit

---
+++ Numero Uno/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Numero Uno
+//
+//  Created by Sam Moorhouse on 14/12/2015.
+//  Copyright © 2015 LittlePlasticSquares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
